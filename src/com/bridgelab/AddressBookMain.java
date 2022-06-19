a08800924427b7aa4e533d65cc38b48be439924b
chandrakalasv <chandrakalasv1502@gmail.com>
2022-06-19 14:05:26 +0530

    added code create contact in address book

--- src/com/bridgelab/AddressBookMain.java
+++ src/com/bridgelab/AddressBookMain.java
@@ -3,5 +3,7 @@ package com.bridgelab;
 public class AddressBookMain {
     public static void main(String[] args) {
         System.out.println("welcome to address book program");
+        AddressBook addressBook = new AddressBook("chandu", "sv","Karnataka", "dvg", 577004, 1223456,"chandu@gmail.com","dvg");
+        System.out.println(addressBook);
     }
 }
