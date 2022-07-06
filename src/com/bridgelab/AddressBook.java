f29d7585af7e1942513c883c1429f8a3b3c0425e
chandrakalasv <chandrakalasv1502@gmail.com>
2022-07-06 17:11:50 +0530

    added code to sort contact by city and state

--- src/com/bridgelab/AddressBook.java
+++ src/com/bridgelab/AddressBook.java
@@ -23,6 +23,7 @@ public class AddressBook {
             System.out.println("Enter 3 to Delete");
             System.out.println("Enter 4 to sort city");
             System.out.println("Enter 5 to sortContactByName");
+            System.out.println("Enter 6 to sortByCityAndState");
             switch (scan.nextInt()) {
                 case 1:
                     add();
@@ -38,6 +39,8 @@ public class AddressBook {
                     break;
                 case 5:
                     sortContactByName();
+                case 6:
+                    sortContactByCityAndState();
                     break;
                 default:
                     status = false;
@@ -163,9 +166,14 @@ public class AddressBook {
     }
 
     public void sortContactByName() {
-        list.stream().sorted((n1,n2)->n1.getFirstName().compareTo(n2.getFirstName())).forEach(System.out::println);
+        list.stream().sorted(Comparator.comparing(Contacts::getFirstName)).forEach(System.out::println);
     }
 
+    public void sortContactByCityAndState() {
+        list.stream().sorted(Comparator.comparing(Contacts::getCity)).forEach(System.out::println);
+        System.out.println();
+        list.stream().sorted(Comparator.comparing(Contacts::getState)).forEach(System.out::println);
+    }
     @Override
     public String toString() {
         return "AddressBook{" +
